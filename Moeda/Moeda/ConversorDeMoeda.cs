2930bbe950367a1232ddab85b77ea0d88442cbeb
BeckenbauerRijiirkd <68623856+BeckenbauerRijiirkd@users.noreply.github.com>
2021-02-04 20:17:02 -0300

    EXercicio Moedas

---
+++ Moeda/Moeda/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace Moeda {
+    class ConversorDeMoeda {
+
+        public static double Conversao(double cotacao, double dolar) {
+            double IOF = 0.06;
+            IOF = (cotacao * dolar) * IOF;
+            double resultado = (cotacao * dolar) + IOF;
+            
+            return resultado;
+        }
+
+    }
+}
---
+++ Moeda/Moeda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moeda {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Qual é a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dolares você vai comprar ?");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.Conversao(cotacao, dolar));
+
+        }
+    }
+}
