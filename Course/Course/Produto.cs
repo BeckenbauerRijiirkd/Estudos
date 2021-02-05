6df72ddd132e1cb1fb86090635a43145471f13b6
BeckenbauerRijiirkd <68623856+BeckenbauerRijiirkd@users.noreply.github.com>
2021-02-04 22:23:44 -0300

    comp

---
+++ Course/Course/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public int quantity;
+
+        public Produto(string nome, double preco, int quantidade) {
+
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public double ValorTotalEmEstoque() {
+
+            return (Preco * Quantidade);
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade = Quantidade + quantidade;
+
+        }
+
+        public void RemoverProdutos(int quantidaderemo) {
+            Quantidade = Quantidade - quantidaderemo;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " Unidades , Valor Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            while (true){
+                Console.WriteLine("Entre com com os dadeos");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preço: ");
+                double preco = double.Parse(Console.ReadLine());
+                
+
+                Produto p = new Produto(nome, preco);
+
+                Produto p3 = new Produto {
+                    Nome = "TV",
+                    Preco = 500.00,
+                    Quantidade = 1
+                };
+
+                
+
+                Console.WriteLine("Dados do produto: " + p.ToString());
+                Console.WriteLine();
+
+                Console.Write("Digite a quantidade a ser adicionada ao estoque: ");
+                int qte = int.Parse(Console.ReadLine());
+                p.AdicionarProdutos(qte);
+
+                Console.WriteLine("Dados do produto Atualizado: " + p.ToString());
+                Console.WriteLine();
+
+                Console.Write("Digite a quantidade a ser removida do estoque: ");
+                int qte2 = int.Parse(Console.ReadLine());
+                p.RemoverProdutos(qte2);
+
+                if (p.Quantidade <= 0) {
+                    Console.WriteLine("Quantidade minima atingida");
+                }
+
+                Console.WriteLine("Dados do produto Atualizado: " + p.ToString());
+            }
+        }
+    }
+}
